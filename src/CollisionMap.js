1dcc6d714c0e984e6429d5ee7c1bdfd15932f961
Swift-Jr <robguardjr@gmail.com>
2018-12-17 08:32:01 +0000

    remove old code

--- src/CollisionMap.js
+++ src/CollisionMap.js
@@ -37,30 +37,4 @@ export class CollisionMap {
       object.updatePosition(x, y, objectPolygon);
     }
   }
-
-  moveTo_bounding(object, x, y) {
-    let objectBounding = object.bounding(x, y);
-
-    let collisionDetected = this.objects
-      .filter(
-        collisionObject => collisionObject.onMap && object !== collisionObject
-      )
-      .some(collisionObject => {
-        let collisionBounding = collisionObject.bounding();
-
-        if (
-          objectBounding.x < collisionBounding.x + collisionBounding.width &&
-          objectBounding.x + objectBounding.width > collisionBounding.x &&
-          objectBounding.y < collisionBounding.y + collisionBounding.height &&
-          objectBounding.height + objectBounding.y > collisionBounding.y
-        ) {
-          return true;
-        }
-        return false;
-      });
-
-    if (!collisionDetected) {
-      object.updatePosition(x, y);
-    }
-  }
 }
